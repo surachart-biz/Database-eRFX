4034030aee4d0b190163222b362ee3d065ad6e34
surachart.t <surachart.tamluang@bizdimension.com>
2025-08-26 10:30:13 +0700

    fix sql

--- erfq-final-init.sql
+++ erfq-final-init.sql
@@ -9,7 +9,9 @@
 -- =============================================
 -- DATABASE CONFIGURATION
 -- =============================================
-CREATE DATABASE erfq_system WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' TABLESPACE = pg_default CONNECTION LIMIT = -1 TEMPLATE template0;
+CREATE ROLE erfx WITH LOGIN PASSWORD 'erfx_2025' SUPERUSER;
+
+CREATE DATABASE erfxdb_dev WITH OWNER = erfx ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8' TABLESPACE = pg_default CONNECTION LIMIT = -1 TEMPLATE template0;
 
 -- Enable Required Extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";    -- สำหรับ generate UUID
